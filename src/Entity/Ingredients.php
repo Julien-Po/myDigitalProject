7243c30a6b0ffc4612bdd60a302847583749f26b
Julien Poirier <poirierjulien30@gmail.com>
2023-11-08 14:40:08 +0100

    ajout des entités provisoires

---
+++ src/Entity/Ingredients.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientsRepository::class)]
+class Ingredients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ingredients')]
+    private ?genre $genre_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGenreId(): ?genre
+    {
+        return $this->genre_id;
+    }
+
+    public function setGenreId(?genre $genre_id): static
+    {
+        $this->genre_id = $genre_id;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
